49e2d1adf70a2df13d3fb70d6819a81ba1d93184
Luka Jaredic <projaredic@gmail.com>
2021-11-10 17:32:25 +0100

    now is backward compatible

--- add_new_user.php
+++ add_new_user.php
@@ -6,6 +6,7 @@
     include './file_functions.php';
     include './users_functions.php';
     require './auth_functions.php';
+    require './country_city_functions.php';
     
     checkAuth();
 
@@ -18,6 +19,9 @@
         $country_id = $_POST['country'];
         $city_id = $_POST['city'];
 
+        if(!isCityInCountry($city_id, $country_id))
+            $city_id = null;
+
         $users = getUsersFromFile(); // fetch from "DB"
         $new_user = [ "id" => nextID($users) , "first_name" => $first_name, "last_name" => $last_name, "email" => $email, "password" => md5($password),"country_id" => $country_id, "city_id" => $city_id];
 
@@ -27,6 +31,12 @@
         // redirect to index with message
         header("location:index.php?user_saved=1");
     }
+
+    function isCityInCountry($city_id, $country_id){
+        $city = getCityByID($city_id);
+        return $city != null && $city["country_id"] == $country_id;
+    }
+  
 
     
 
